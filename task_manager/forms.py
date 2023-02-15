a55ad39cb1782eb69e3a9733e375f2c1f47b6408
Vadim Gusak <gusakvd@gmail.com>
2023-02-15 18:34:38 +1000

    Add new files

---
+++ task_manager/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name' , 'last_name', )
